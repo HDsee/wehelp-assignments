87b2f482ffaaf7fbc6b94d1f2ad816914005ce9f
HDsee <93437400+HDsee@users.noreply.github.com>
2022-01-18 21:43:26 +0800

    Update week-4.py

--- week-4/week-4.py
+++ week-4/week-4.py
@@ -1,11 +1,9 @@
 #載入 Flask 所有相關的工具
-from email import message
 from flask import Flask
 from flask import request
 from flask import redirect
 from flask import render_template
 from flask import session
-from flask import url_for
 #建立 Application 物件，設定靜態檔案的處理
 app=Flask(__name__, static_folder="static", static_url_path="/")
 
