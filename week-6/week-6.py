b3dada70608c2cf34d06522039033cc8a38b537d
HDsee <93437400+HDsee@users.noreply.github.com>
2022-02-09 19:12:26 +0800

    Create week-6.py

---
+++ week-6/week-6.py
@@ -0,0 +1,83 @@
+#載入 Flask 所有相關的工具
+from flask import Flask
+from flask import request
+from flask import redirect
+from flask import render_template
+from flask import session
+
+import mysql.connector
+
+db = mysql.connector.connect(
+    host='localhost',
+    port='3306',
+    user='root',
+    password='root',
+    database='website'
+)
+
+cursor = db.cursor()
+
+#建立 Application 物件，設定靜態檔案的處理
+app=Flask(__name__, static_folder="static", static_url_path="/")
+
+app.secret_key="HD"
+
+@app.route('/')
+def index():
+    if 'user' in session:
+        return redirect('/member')
+    return render_template('index.html')
+
+@app.route('/signup', methods=["POST"])
+def signup():
+    name=request.form['username']
+    account=request.form['newaccount']
+    password=request.form['newpassword']
+    
+    cursor.execute('select * from member where usename=%s',(account,))
+    user = cursor.fetchall()
+
+    if (name == '') or (account == '') or (password == '') :
+        return redirect("/error?message=請輸入帳號、密碼")
+    if user:
+        return redirect("/error?message=帳號已經被註冊")
+    else:
+        cursor.execute('INSERT INTO `member` (name,usename,password) VALUES (%s,%s,%s)',(name,account,password))
+        db.commit()
+        return redirect('/')
+    
+
+@app.route('/signin', methods=["POST"])
+def signin():
+    account=request.form['account']
+    password=request.form['password']
+    cursor.execute('select * from member where usename=%s and password=%s',(account,password))
+    user = cursor.fetchall()
+    userdata=(user[0])
+    username=userdata[1]
+    if user:
+        session['user'] = account
+        session['name'] = username
+        return redirect('/member')
+    if (account == '') or (password == ''):
+        return redirect("/error?message=請輸入帳號、密碼")
+    return redirect("/error?message=帳號、密碼輸入錯誤")
+    
+@app.route('/member')
+def member():
+    if 'user' in session:
+        return render_template('member.html', name=session['name'])
+    return redirect('/index')
+
+@app.route('/error')
+def error():
+    message=request.args.get("message","")
+    return render_template('error.html',msg=message)
+
+@app.route('/signout')
+def signout():
+    session.pop('user')
+    return redirect('/')
+
+#指定埠號
+app.run (port=3000)
