b7ff67653bc03c6d738846a3b9d108dde017d8cf
HDsee <93437400+HDsee@users.noreply.github.com>
2022-02-20 15:50:40 +0800

    Update member.js

--- week-7/static/member.js
+++ week-7/static/member.js
@@ -1,4 +1,5 @@
 const userSearchForm = document.querySelector('.search-user')
+const changeUsernameForm = document.querySelector('.change-username')
 
 // 查詢會員姓名
 function searchUser(find){
@@ -14,10 +15,34 @@ function searchUser(find){
             searchResult.innerText = `${name}(${username})`
         })
         .catch(error =>{
-                searchResult.innerText = '無此會員'
+            searchResult.innerText = '無此會員'
         })
 }
 
-
+// 更新會員姓名
+function changeUsername(change){
+    change.preventDefault()
+    const url = '/api/member'
+    const newName = this.querySelector('input').value
+    const changeResult = this.querySelector('p')
+    fetch(url, {
+        method: 'POST',
+        headers:{
+            'Contnet-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            'name': `${newName}`
+        })
+    })
+        .then(res => res.json())
+        .then(data => {
+            if(data['ok']){
+                changeResult.innerText = '更新成功'
+            }else{
+                changeResult.innerText = '更新失敗'
+            }
+        })
+}
 
 userSearchForm.addEventListener('submit', searchUser)
+changeUsernameForm.addEventListener('submit', changeUsername)
